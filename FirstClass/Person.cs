cc17a588a53521fee31c8defe4690ddb8884fc83
auro <auro@DESKTOP-AV8I9BT>
2024-08-07 15:33:09 +0300

    Add project files.

---
+++ FirstClass/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstClass
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public void Student()
+        {
+            Console.WriteLine($"Öğrencinin adı: {FirstName} \nSoyadı: {LastName} \nDoğum tarihi: {BirthDate.ToShortDateString()}");
+        }
+        public void Teacher()
+        {
+            Console.WriteLine($"Öğretmenin adı: {FirstName} \nSoyadı: {LastName} \nDoğum tarihi: {BirthDate.ToShortDateString()}");
+        }
+    }
+}
---
+++ FirstClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using FirstClass;
+
+Person student1 = new Person()
+{
+    FirstName = "Atakan",
+    LastName = "Çelik",
+    BirthDate = new DateTime(1998, 11, 04)
+
+};
+Person student2 = new Person()
+{
+    FirstName = "Ahmet",
+    LastName = "Kılıç",
+    BirthDate = new DateTime(1998, 08, 02)
+
+};
+Person teacher = new Person()
+{
+    FirstName = "Mustafa",
+    LastName = "Yılmaz",
+    BirthDate = new DateTime(1982, 01, 05)
+
+};
+Console.WriteLine("--------Öğrencilerimiz--------");
+student1.Student();
+student2.Student();
+Console.WriteLine("--------Öğretmenimiz--------");
+teacher.Teacher();
